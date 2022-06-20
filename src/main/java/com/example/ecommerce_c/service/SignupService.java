7f4f8f9f6c83936ada320317ea48f44b3da8bf96
dainateranishi <daina.teranishi@gmail.com>
2022-06-20 13:56:08 +0900

    checkSamaMailAddressの完成

--- src/main/java/com/example/ecommerce_c/service/SignupService.java
+++ src/main/java/com/example/ecommerce_c/service/SignupService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.ecommerce_c.domain.User;
 import com.example.ecommerce_c.repository.UserRepository;
 
 /**
@@ -19,5 +20,14 @@ public class SignupService {
 	@Autowired
 	private UserRepository userRepository;
 	
-	
+	/**
+	 * 同じメールアドレスが存在していないかを確認する.
+	 * 
+	 * @param email 確認するメールアドレス
+	 * @return 存在していればそのUserクラス、存在していなければnull
+	 */
+	public User checkSameMailAddress(String email) {
+		
+		return userRepository.findByMailAddress(email);
+	}
 }
