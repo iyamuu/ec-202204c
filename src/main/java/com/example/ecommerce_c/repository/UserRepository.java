59ed33d7ab3abe488722f95fc373aef5a2f3a796
dainateranishi <daina.teranishi@gmail.com>
2022-06-20 13:40:41 +0900

    UserRepository RowMapperの追加

--- src/main/java/com/example/ecommerce_c/repository/UserRepository.java
+++ src/main/java/com/example/ecommerce_c/repository/UserRepository.java
@@ -1,5 +1,36 @@
 package com.example.ecommerce_c.repository;
 
-public class UserRepository {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import com.example.ecommerce_c.domain.User;
+
+/**
+ * Usersテーブルを操作するRepositoryクラス.
+ * 
+ * @author daina.teranishi
+ *
+ */
+@Repository
+public class UserRepository {
+	
+	@Autowired
+	private NamedParameterJdbcTemplate jdbcTemplate;
+	
+	
+	private RowMapper<User> USER_ROW_MAPPER = (rs, rowNumber)->{
+		
+		User user = new User();
+		user.setId(rs.getInt("id"));
+		user.setName(rs.getString("name"));
+		user.setEmail(rs.getString("email"));
+		user.setPassword(rs.getString("password"));
+		user.setZipCode(rs.getString("zipcode"));
+		user.setAddress(rs.getString("address"));
+		user.setTelephone(rs.getString("telephone"));
+		
+		return user;
+	};
 }
