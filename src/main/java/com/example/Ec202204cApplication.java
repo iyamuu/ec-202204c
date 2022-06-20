3c095fcd18197f4d0b380c2a767a85eb6b137dfb
kouki-matsuura <kouki19991222@iCloud.com>
2022-06-20 11:23:17 +0900

    first commit

---
+++ src/main/java/com/example/Ec202204cApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec202204cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec202204cApplication.class, args);
+	}
+
+}
