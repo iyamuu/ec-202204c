af049737c4ef0c19b01774b4090ab037d099db12
fukube-satoshi <al18054@shibaura-it.ac.jp>
2022-06-20 16:15:04 +0900

    SQL文を修正

---
+++ src/main/java/com/example/ecommerce_c/service/OrderService.java
@@ -0,0 +1,51 @@
+package com.example.ecommerce_c.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ecommerce_c.domain.Order;
+import com.example.ecommerce_c.repository.OrderRepository;
+
+/**
+ * 注文を操作するサービス
+ * 
+ * @author ryuya.sasagawa
+ *
+ */
+@Service
+public class OrderService {
+	@Autowired
+	private OrderRepository repository;
+	
+	/**
+	 * 注文を検索する
+	 * 
+	 * @param id ID
+	 * @return 注文 存在しない場合はnullを返す
+	 */
+	public Order findById(int id) {
+		Order order = repository.findById(id);
+		return order;
+	}
+
+	/**
+	 * 注文を登録する.
+	 * 
+	 * @param order 注文
+	 * @return 登録した行のID
+	 */
+	public int insert(Order order) {
+		int id = repository.insert(order);
+		return id;
+	}
+
+	/**
+	 * 注文内容を更新する.
+	 * 
+	 * @param order 注文
+	 */
+	public void update(Order order) {
+		repository.update(order);
+	}
+
+}
