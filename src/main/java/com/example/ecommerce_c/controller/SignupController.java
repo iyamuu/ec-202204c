5ee76532b09ade78d273256ab88b956334a939eb
dainateranishi <daina.teranishi@gmail.com>
2022-06-20 14:18:36 +0900

    SignupController registerUserの完成

--- src/main/java/com/example/ecommerce_c/controller/SignupController.java
+++ src/main/java/com/example/ecommerce_c/controller/SignupController.java
@@ -1,9 +1,15 @@
 package com.example.ecommerce_c.controller;
 
+import org.graalvm.compiler.lir.LIRInstruction.Use;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.ecommerce_c.domain.User;
 import com.example.ecommerce_c.form.UserForm;
 import com.example.ecommerce_c.service.SignupService;
 
@@ -18,6 +24,30 @@ public class SignupController {
 	public String getSignupPage(UserForm form) {
 		
 		return "login/signup";
+	}
+	
+	@PostMapping("/signup")
+	public String registerUser(@Validated UserForm form, BindingResult result) {
+		
+		//emailの重複チェック、存在していればバリデーション結果にエラーを追加
+		User existsUser = signupService.checkSameMailAddress(form.getEmail());
+		if(existsUser != null) {
+			result.rejectValue("email", null, "このメールアドレスは既に存在しています");
+		}
+		
+		
+		if(result.hasErrors()) {
+			return getSignupPage(form);
+		}
+		
+		
+		User newUser = new User();
+		BeanUtils.copyProperties(form, newUser);
+		newUser = signupService.registerUser(newUser);  //登録処理、ここでidが付与される
+		
+		
+		return "login/login";
+		
 	}
 	
 }
