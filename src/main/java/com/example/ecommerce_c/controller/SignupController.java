6020a8171dbe2e78c6aa46d5997b7a65dad6d7b3
dainateranishi <daina.teranishi@gmail.com>
2022-06-20 14:11:45 +0900

    getSignupPageの完成

--- src/main/java/com/example/ecommerce_c/controller/SignupController.java
+++ src/main/java/com/example/ecommerce_c/controller/SignupController.java
@@ -2,7 +2,9 @@ package com.example.ecommerce_c.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.ecommerce_c.form.UserForm;
 import com.example.ecommerce_c.service.SignupService;
 
 @Controller
@@ -11,5 +13,11 @@ public class SignupController {
 	@Autowired
 	private SignupService signupService;
 	
+	
+	@GetMapping("/signup")
+	public String getSignupPage(UserForm form) {
+		
+		return "login/signup";
+	}
 	
 }
