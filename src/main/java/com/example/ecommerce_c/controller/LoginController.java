9b25cb0847c439a320dc131abca40574034ce7d1
dainateranishi <daina.teranishi@gmail.com>
2022-06-21 09:45:17 +0900

    ログイン機能の完成

--- src/main/java/com/example/ecommerce_c/controller/LoginController.java
+++ src/main/java/com/example/ecommerce_c/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce_c.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -7,7 +8,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.ecommerce_c.domain.User;
 import com.example.ecommerce_c.form.LoginForm;
+import com.example.ecommerce_c.service.LoginService;
 
 /**
  * ログイン処理を行うCOntrollerクラス
@@ -17,18 +20,40 @@ import com.example.ecommerce_c.form.LoginForm;
  */
 @Controller
 public class LoginController {
-	
+
+	@Autowired
+	private LoginService loginService;
+
 	/**
 	 * ログインページを表示する.
 	 * 
-	 * @param form ログインフォーム
-	 * @param model　リクエストスコープ
+	 * @param form  ログインフォーム
+	 * @param model リクエストスコープ
 	 * @return ログインページのパス
 	 */
 	@GetMapping("/login")
 	public String getLoginPage(LoginForm form, Model model) {
-		
+
 		return "login/login";
 	}
-	
+
+	@PostMapping("/login")
+	public String login(@Validated LoginForm form, BindingResult result, Model model) {
+
+		// formのバリデーションチェック
+		if (result.hasErrors()) {
+			return getLoginPage(form, model);
+		}
+
+		// ログイン成功したかのチェック
+		User loginUser = loginService.login(form.getEmail(), form.getPassword());
+		if (loginUser == null) {
+			model.addAttribute("loginStatus", "メールアドレスかパスワードが異なります");
+			return getLoginPage(form, model);
+		}
+
+		model.addAttribute("loginStatus", "ログインに成功しました");
+		return "login/login";
+
+	}
 }
