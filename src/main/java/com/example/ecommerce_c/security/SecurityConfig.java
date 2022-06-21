50930670f514676cc1d5795259a2974b59b06bfc
dainateranishi <daina.teranishi@gmail.com>
2022-06-21 14:10:03 +0900

    Formログインの修正

--- src/main/java/com/example/ecommerce_c/security/SecurityConfig.java
+++ src/main/java/com/example/ecommerce_c/security/SecurityConfig.java
@@ -1,41 +1,42 @@
 package com.example.ecommerce_c.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private FormLoginService formLoginService;
-	
+
 	/** セキュリティの対象外を設定 */
-	public void cofigure(WebSecurity web) throws Exception{
-		
-		web.ignoring().antMatchers("/js/**").antMatchers("/css/**");
-	}
-	
 	@Override
-	public void configure(HttpSecurity http) throws Exception{
-		
-		http.authorizeHttpRequests().antMatchers("/login").permitAll()
-			.antMatchers("/signup").permitAll().anyRequest().authenticated()
-			.and()
-			.formLogin()
-			.loginPage("/login").loginProcessingUrl("/login")  //ログインボタンのURL
-			.failureForwardUrl("/login?error=true")
-			.defaultSuccessUrl("/login", false);
+	public void configure(WebSecurity web) throws Exception {
+		// セキュリティを適用しない
+		web.ignoring().antMatchers("/css/**").antMatchers("/js/**").antMatchers("/img/**");
 	}
-	
-	
+
 	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception{
-		
+	public void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/login").permitAll().antMatchers("/signup").permitAll().anyRequest()
+				.authenticated().and().formLogin().loginPage("/login").loginProcessingUrl("/login") // ログインボタンのURL
+				.failureForwardUrl("/login?error=true").defaultSuccessUrl("/login", false)
+				.usernameParameter("email").passwordParameter("password");
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
 		auth.userDetailsService(formLoginService).passwordEncoder(new BCryptPasswordEncoder());
 	}
-	
-	
+
 }
