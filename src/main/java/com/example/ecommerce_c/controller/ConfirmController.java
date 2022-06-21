8a3230c263e05e90cd2f2de54e6503f15ee1965e
fukube-satoshi <al18054@shibaura-it.ac.jp>
2022-06-21 09:48:58 +0900

    注文確認画面を表示する機能を追加

--- src/main/java/com/example/ecommerce_c/controller/ConfirmController.java
+++ src/main/java/com/example/ecommerce_c/controller/ConfirmController.java
@@ -2,11 +2,14 @@ package com.example.ecommerce_c.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.ecommerce_c.domain.Order;
 import com.example.ecommerce_c.domain.User;
-import com.example.ecommerce_c.service.OrderService;
+import com.example.ecommerce_c.form.ConfirmForm;
+import com.example.ecommerce_c.service.ConfirmService;
 
 /**
  * 注文確認画面を操作するコントローラー.
@@ -18,57 +21,39 @@ import com.example.ecommerce_c.service.OrderService;
 @RequestMapping("/confirm")
 public class ConfirmController {
 	@Autowired
-	private OrderService orderService;
-	
-//	@Autowired
-//	private UserService userService;
-	
+	private ConfirmService service;
+
+	@ModelAttribute
+	public ConfirmForm setUpConfirmForm() {
+		return new ConfirmForm();
+	}
+
 	/**
-	 * 注文確認画面を表示する.
-	 * ログインしていなかったらログインページに遷移する。
+	 * 注文確認画面を表示する. ログインしていなければログイン画面に遷移する。
 	 * 
-	 * @param orderId ID
-	 * @return 注文確認画面
+	 * @param orderId 注文ID
+	 * @param model   モデル
+	 * @return 注文確認画面/ログイン画面
 	 */
 	@RequestMapping("")
-	public String showConfirm(int orderId) {
-//		FIXME: ユーザードメインやユーザーサービスが実装されないとログインしているかの判定ができない
-		Order order = orderService.findById(orderId);
-		
-//		User user = userService.findById(order.getUserId());
-//		
-//		if(user == null) {
-//			return "redirect:/login";
-//		}
-		
+	public String showConfirm(int orderId, Model model) {
+		Order order = service.searchOrder(orderId);
+		User user = service.searchUser(order.getUserId());
+
+		if (user == null) {
+			return "login/login";
+		}
+
+//		たぶん必要
+		model.addAttribute("orderId", orderId);
+//		注文商品一覧
+		model.addAttribute("orderItemList", service.stab_searchOrderItemByOrderId(orderId));
+
 		return "order_confirm";
 	}
 	
-//	リポジトリとサービスの動作確認
-	/**
-	 * サービスの動作確認をする
-	 * 
-	 * @return 注文確認画面
-	 */
-//	@RequestMapping("/test")
-	public String test() {
-//		WORNING: テスト用。insertやupdateが行われるため注意！テストするときはRequestMappingのコメントアウトを外してください。
-		System.out.println("orderService.findById (expected 'null'): " + orderService.findById(1));
-		Order order = new Order();
-		order.setUserId(12345);
-		order.setStatus(0);
-		order.setTotalPrice(0);
-		
-		System.out.println("totalPrice (expected '0'): " + order.getCalcTotalPrice());
-		System.out.println("tax (expected '0'): " + order.getTax());
-
-		System.out.println("orderService.insert (expected '1')" + orderService.insert(order));
-		Order order2 = orderService.findById(1);
-		System.out.println("orderService.findById (expected 'order[id=1, ...]'): " + order2);
-		order2.setTotalPrice(10000);
-		orderService.update(order2);
-		System.out.println("update complate.");
-		System.out.println("orderService.findById (expected 'order[id=1, ... , totalPrice=10000, ...]'): " + orderService.findById(1));
-		return "order_confirm";
+	@RequestMapping("/test")
+	public String stab_showCartList() {
+		return "cart_list";
 	}
 }
