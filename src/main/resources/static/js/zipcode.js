b7018cb0042bcc994afb6572eb1bcfa8bc9b6b10
dainateranishi <daina.teranishi@gmail.com>
2022-06-22 10:51:56 +0900

    郵便番号のオートコンプリート

---
+++ src/main/resources/static/js/zipcode.js
@@ -0,0 +1,52 @@
+"use strict";
+
+$(function () {
+  //郵便番号検索欄が変更されたとき
+  $("#zipCode").keyup(function () {
+    //入力されたものを検知するにはKeyup
+    if ($("#zipCode").val() !== "") {
+      let zipCode = $("#zipCode").val().replace("-", "");
+      $.ajax({
+        url: "https://zipcoda.net/api",
+        dataType: "jsonp",
+        data: {
+          zipcode: zipCode,
+        },
+        async: true,
+      })
+        .done(function (data) {
+          console.dir(JSON.stringify(data));
+          $("#zipList").empty();
+          data.items.forEach((item) => {
+            $("#zipList").append(
+              "<option class='zipSuggestion' value=" +
+                item.zipcode +
+                ":" +
+                item.components.join("") +
+                ">"
+            );
+          });
+        })
+        .fail(function (XMLHttpRequest, textStatus, errorThrown) {
+          alert("通信が失敗しました");
+          console.log("XMLHttpRequest" + XMLHttpRequest);
+          console.log("textStatus" + textStatus);
+          console.log("errorThrown" + errorThrown);
+        });
+    }
+  });
+
+  $("#zipCode").change(function () {
+    console.log($("#zipCode").val());
+    let addressWithCode = $("#zipCode").val().split(":");
+    let zipcode =
+      addressWithCode[0].slice(0, 3) + "-" + addressWithCode[0].slice(3, 7);
+    let address = addressWithCode[1];
+
+    // console.log(zipCode);
+    // console.log(address);
+
+    $("#zipCode").val(zipcode);
+    $("#address").val(address);
+  });
+});
