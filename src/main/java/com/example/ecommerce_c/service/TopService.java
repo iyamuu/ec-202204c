2141aabfea6d76b69c0b9507fb6cc58134d4d918
fukube-satoshi <al18054@shibaura-it.ac.jp>
2022-06-22 13:05:56 +0900

    top画面に遷移するとカートの中身が空になってしまう不具合を修正

--- src/main/java/com/example/ecommerce_c/service/TopService.java
+++ src/main/java/com/example/ecommerce_c/service/TopService.java
@@ -6,13 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.ecommerce_c.domain.Item;
+import com.example.ecommerce_c.domain.Order;
 import com.example.ecommerce_c.repository.ItemRepository;
+import com.example.ecommerce_c.repository.OrderRepository;
 
 @Service
 public class TopService {
 
 	@Autowired
 	ItemRepository itemRepository;
+
+	@Autowired
+	private OrderRepository orderRepository;
 
 	/**
 	 * アイテムを取得する処理.
@@ -27,5 +32,27 @@ public class TopService {
 			return itemRepository.findPages(from, to);
 		}
 		return itemRepository.findByName(from, to, name);
+	}
+
+	/**
+	 * ユーザーIDから注文を検索する.
+	 * 
+	 * @param userId ユーザーID
+	 * @return 注文ID 初ログインorゲストならnull
+	 */
+	public Integer searchOrderByUserId(int userId) {
+		Integer orderId = orderRepository.findOrderIdByUserId(userId);
+		return orderId;
+	}
+
+	/**
+	 * 注文を追加する.
+	 * 
+	 * @param order 注文
+	 * @return 追加したOrderのID
+	 */
+	public int insertOrder(Order order) {
+		int returningId = orderRepository.insert(order);
+		return returningId;
 	}
 }
