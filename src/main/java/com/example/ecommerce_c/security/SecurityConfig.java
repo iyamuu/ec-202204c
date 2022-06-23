dd7e06a3263d3e4518db2b6553da586767afda87
Daina Teranishi <daina.teranishi@gmail.com>
2022-06-23 21:11:50 +0900

    SecurityConfigへOAuth2の設定追加

--- src/main/java/com/example/ecommerce_c/security/SecurityConfig.java
+++ src/main/java/com/example/ecommerce_c/security/SecurityConfig.java
@@ -10,6 +10,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -35,9 +39,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/getItemByPage").permitAll()
 				.antMatchers("/show").permitAll()
 				.antMatchers("/add").permitAll().antMatchers("/delete").permitAll().antMatchers("/update").permitAll() // RestControllerへも許可
-				.anyRequest().authenticated().and().formLogin().loginPage("/login").loginProcessingUrl("/login") // ログインボタンのURL
+				.anyRequest().authenticated()
+				.and().formLogin().loginPage("/login").loginProcessingUrl("/login") // ログインボタンのURL
 				.failureForwardUrl("/login?error=true").defaultSuccessUrl("/top", false).usernameParameter("email")
-				.passwordParameter("password");
+				.passwordParameter("password")
+				.and().oauth2Login() // Line Login
+				.loginPage("/login");
 
 		
 		//cookieにCSRFトークンを入れる
@@ -52,6 +59,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 
 		auth.userDetailsService(formLoginService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	@Bean // Line認証プロバイダのBean定義
+	public ClientRegistrationRepository clientRegistrationRepository() {
+		return new InMemoryClientRegistrationRepository(ClientRegistration.withRegistrationId("line")
+				.clientId("1657245742").clientSecret("9956e938e9df0ab9516829d013bf4ea0")
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE).scope("profile").clientName("LINE")
+				.authorizationUri("https://access.line.me/oauth2/v2.1/authorize")
+				.tokenUri("https://api.line.me/oauth2/v2.1/token").jwkSetUri("https://api.line.me/oauth2/v2.1/certs")
+				.userInfoUri("https://api.line.me/v2/profile").userNameAttributeName("userId")
+				.redirectUri("https://ec-202204c-toy.herokuapp.com/ec-202204c/oauth2/code/line").build());
 	}
 
 	@Bean
